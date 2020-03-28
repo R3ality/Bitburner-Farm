08df8aa70a5213526a016c30861e04409588fa8e
R3ality <idle.relay@gmail.com>
2020-03-29 01:39:39 +0200

    Formatting

--- _init.script.js
+++ _init.script.js
@@ -1,6 +1,13 @@
 // Download the other relevant scripts
 
-var fileNames = ["_farm.script", "_farm-local.script", "_grow.script", "_hack.script", "_prep.script", "_weak.script"];
+var fileNames = [
+    "_farm.script",
+    "_farm-local.script",
+    "_grow.script",
+    "_hack.script",
+    "_prep.script",
+    "_weak.script"
+];
 
 fileNames.forEach(function(fileName) {
     rm(fileName); // wget() should overwrite but this did not appear to be working
