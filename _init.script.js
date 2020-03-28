9775a44d3e6b45ae26eef3a616d5657d0a092417
R3ality <idle.relay@gmail.com>
2020-03-29 00:31:07 +0200

    Add _init.script

---
+++ _init.script.js
@@ -0,0 +1,8 @@
+// Download the other relevant scripts
+
+var fileNames = ["_farm.script", "_grow.script", "_hack.script", "_prep.script", "_weak.script"];
+
+fileNames.forEach(function(fileName) {
+    var url = "https://raw.githubusercontent.com/R3ality/Bitburner-Farm/master/" + fileName + ".js";
+    wget(url, fileName);
+});
