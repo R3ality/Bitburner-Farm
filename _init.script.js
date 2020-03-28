03c9eb7f25baaba4a8d5470a0f2a074b6561e9bd
R3ality <idle.relay@gmail.com>
2020-03-29 01:19:46 +0200

    Remove file before wget

--- _init.script.js
+++ _init.script.js
@@ -3,6 +3,7 @@
 var fileNames = ["_farm.script", "_farm-local.script", "_grow.script", "_hack.script", "_prep.script", "_weak.script"];
 
 fileNames.forEach(function(fileName) {
+    rm(fileName); // wget() should overwrite but this did not appear to be working
     var url = "https://raw.githubusercontent.com/R3ality/Bitburner-Farm/master/" + fileName + ".js";
     wget(url, fileName);
 });
