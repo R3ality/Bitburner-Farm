5c8cc620f728ad72db23e52d8a4629eea4e7b29a
R3ality <idle.relay@gmail.com>
2020-03-29 21:35:03 +0300

    Add _program-manager

---
+++ _program-manager.ns.js
@@ -0,0 +1,50 @@
+// Proram manager for purchasing software tools off the darkweb
+
+// Referenced or copied sources:
+// https://github.com/MercuriusXeno/BitBurnerScripts/blob/master/program-manager.js
+
+export async function main(ns) {
+    const programNames = ["BruteSSH.exe", "FTPCrack.exe", "relaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe", "DeepscanV1.exe", "DeepscanV2.exe", "Autolink.exe"];
+    const programCosts = [500000, 1500000, 5000000, 30000000, 250000000, 500000, 25000000, 1000000];
+
+    var hasAllPrograms = false;
+    while (true) {
+        if (hasAllPrograms) break;
+        if (!hasTor(ns)) {
+            await ns.sleep(5000);
+            continue;
+        }
+        var foundMissingProgram = false;
+        for (var i = 0; i < programNames.length; ++i) {
+            var prog = programNames[i];
+            if (hasProgram(ns, prog)) {
+                continue;
+            } else {
+                foundMissingProgram = true;
+            }
+            var cost = programCosts[i];
+            if (cost <= getPlayerMoney(ns)) {
+                ns.tprint("<font color=cyan> NOTIFY:</font> Purchasing program " + prog + " for " + ns.nFormat(cost, "$0.000a") + " money");
+                ns.purchaseProgram(prog);
+            }
+        }
+        if (!foundMissingProgram) {
+            hasAllPrograms = true;
+            ns.tprint("<font color=green>SUCCESS:</font> All predefined programs already available. Exiting..");
+        }
+        await ns.sleep(5000);
+    }
+}
+
+function getPlayerMoney(ns) {
+    return ns.getServerMoneyAvailable("home");
+}
+
+function hasProgram(ns, program) {
+    return ns.fileExists(program, "home");
+}
+
+function hasTor(ns) {
+    var homeNodes = ns.scan("home");
+    return homeNodes.includes("darkweb");
+}
